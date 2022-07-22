430cee7621cfda7a68cc42e0b8c365eb4de17e10
zjouahri <zakaria.jouahri@capgemini.com>
2022-07-22 14:08:54 +0200

    Initial spring boot project

---
+++ src/test/java/ziko/spring/in28min/DemoApplicationTests.java
@@ -0,0 +1,12 @@
+package ziko.spring.in28min;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
